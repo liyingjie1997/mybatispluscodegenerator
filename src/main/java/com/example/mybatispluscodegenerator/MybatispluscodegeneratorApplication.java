7deb6f38fa5244781f8398592465235864b5730e
liyingjie <15613173857@163.com>
2022-04-19 14:52:41 +0800

    init

---
+++ src/main/java/com/example/mybatispluscodegenerator/MybatispluscodegeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.mybatispluscodegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatispluscodegeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatispluscodegeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mybatispluscodegenerator/MybatispluscodegeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybatispluscodegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatispluscodegeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
